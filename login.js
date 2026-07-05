d15242eeb79ab6faf126a873083dcca609694d53
Mettu Sai Harshitha <vyshu1426@gmail.com>
2026-07-05 10:34:19 +0530

    Debug login

--- login.js
+++ login.js
@@ -1,28 +1,56 @@
-async function login(){
+async function login() {
 
-const email=document.getElementById("email").value;
-const password=document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-console.log(email,password);
+    console.log(email, password);
 
-const res=await fetch(
-"https://londonkids-backend.onrender.com/api/login",
-{
-method:"POST",
-headers:{
-"Content-Type":"application/json"
-},
-body:JSON.stringify({
-email,
-password
-})
-}
-);
+    try {
 
-console.log("Status:",res.status);
+        const res = await fetch(
+            "https://londonkids-backend.onrender.com/api/login",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+            }
+        );
 
-const data=await res.json();
+        console.log("Status:", res.status);
 
-console.log(data);
+        const data = await res.json();
+
+        console.log(data);
+
+        if (data.success) {
+
+            localStorage.setItem(
+                "adminToken",
+                data.token
+            );
+
+            console.log(
+                localStorage.getItem("adminToken")
+            );
+
+            window.location.href = "admin.html";
+
+        } else {
+
+            alert("Invalid Email or Password");
+
+        }
+
+    } catch (err) {
+
+        console.error(err);
+        alert("Server Error");
+
+    }
 
 }
